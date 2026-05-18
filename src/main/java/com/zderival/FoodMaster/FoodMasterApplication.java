b300578d3a09164766a5836d8982189ff85a8c43
Zachary Derival <zderival@gmail.com>
2026-05-17 22:09:25 -0400

    Beginning stages of FoodMaster program

---
+++ src/main/java/com/zderival/FoodMaster/FoodMasterApplication.java
@@ -0,0 +1,13 @@
+package com.zderival.FoodMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zderival/FoodMaster/FoodMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zderival.FoodMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
